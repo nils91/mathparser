3afbc36521b3ca53b1bed8d3c906b84642bc6bec
Nils Dralle <nils.91@googlemail.com>
2020-06-30 16:29:12 +0200

    delete old sample classes

--- term/src/main/java/IVisitor.java
+++
@@ -1,9 +0,0 @@
-/**
- * Testinterface for Visitor-Pattern. Example for thesis. Not used in actual application.
- * @author Nils Dralle
- *
- * @param <T>
- */
-public interface IVisitor<T> {
-	T visit(A obj);
-}
