f38d516f2c139314f2a1298cc9a22bbfffdca566
Nils Dralle <nils.91@googlemail.com>
2020-12-08 15:29:57 +0100

    completed script for connection testing

--- test-deploy-docker/test-mathparser-request.sh
+++ test-deploy-docker/test-mathparser-request.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+http_code=0
 for i in 1 2 3 4 5 6
 do
-   echo "Welcome $i times"
+   http_code=$(curl -s -o /dev/null -i -w "%{http_code}" -X POST "http://localhost:8080/MathParserDev/test/ping" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"id\":0}")
+   if [ $http_code -eq 200 ]
+   then
+      exit 0
+   fi
+   sleep 1s
 done
+exit 1
