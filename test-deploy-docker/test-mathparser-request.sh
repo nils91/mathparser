53dfa47d8d2d78386e6a2e6af853361c7edfd3e9
Nils Dralle <nils.91@googlemail.com>
2020-12-08 15:44:30 +0100

    action finished

--- test-deploy-docker/test-mathparser-request.sh
+++ test-deploy-docker/test-mathparser-request.sh
@@ -5,8 +5,10 @@ do
    http_code=$(curl -s -o /dev/null -i -w "%{http_code}" -X POST "http://localhost:8080/MathParserDev/test/ping" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"id\":0}")
    if [ $http_code -eq 200 ]
    then
+      echo Deployed correctly
       exit 0
    fi
    sleep 1s
 done
+echo Not deployed correctly
 exit 1
