e8e44fb3c81cfa75740c0b2d5fd117a55b849dd3
Nils Dralle <nils.91@googlemail.com>
2020-12-10 16:57:24 +0100

    changed test script and action to connect on 127.0.0.1 only

--- test-deploy-docker/test-mathparser-request.sh
+++ test-deploy-docker/test-mathparser-request.sh
@@ -2,7 +2,7 @@
 http_code=0
 for i in 1 2 3 4 5 6
 do
-   http_code=$(curl -s -o /dev/null -i -w "%{http_code}" -X POST "http://localhost:8080/MathParserDev/test/ping" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"id\":0}")
+   http_code=$(curl -s -o /dev/null -i -w "%{http_code}" -X POST "http://127.0.0.1:8080/MathParserDev/test/ping" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"id\":0}")
    if [ $http_code -eq 200 ]
    then
       echo Deployed correctly
